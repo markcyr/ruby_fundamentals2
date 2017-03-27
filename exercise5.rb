9c723ff60b256535353ea66e00ef6c08390059ca
markcyr <markcyr@msn.com>
2017-03-27 15:24:54 -0400

    name backward twice

--- exercise5.rb
+++ exercise5.rb
@@ -2,4 +2,4 @@ puts "What does your name look like backwards"
 puts "What's your name"
 user_input = gets.chop
 puts "Let's see "
-puts "Hello #{user_input.reverse}! Welcom home."
+puts "Hello #{user_input.reverse}#{user_input.reverse}! Welcom home."
