cce37ec70020ba04e989b1b78a1fae1463a5c21f
markcyr <markcyr@msn.com>
2017-03-28 16:04:55 -0400

    hash exercise10 increased class size

--- exercise10.rb
+++ exercise10.rb
@@ -7,3 +7,29 @@ students = {
 students.each do |cohort, number|
 puts "#{cohort}: #{number} students"
 end
+
+students[:cohort4]=43
+
+puts "We're super succesfully. We've add a new cohort."
+students.each do |cohort, number|
+puts "Here is the list  #{cohort}"
+end
+
+puts "We're super succesfully. We've got more students"
+students.each do |cohort, number|
+#  puts "Here is the list  #{number}"
+  number_new = number * 1.05
+  #  puts "Here is the list  #{number.to_i} with the new numbers"
+    students[cohort]= number_new.to_i
+end
+
+puts "New list"
+students.each do |cohort, number|
+puts "Here is the list  #{cohort} : #{number}"
+end
+
+
+# puts "new numbers"
+# students.each do |cohort, number|
+# puts "Here are the numbers #{number}"
+# end
