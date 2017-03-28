da1e1fc6cac0509522608312cc7bbc86e2f3fc0e
markcyr <markcyr@msn.com>
2017-03-28 16:24:52 -0400

    hash exercise10 complete

--- exercise10.rb
+++ exercise10.rb
@@ -3,18 +3,18 @@ students = {
   :cohort2 => 42,
   :cohort3 => 22
 }
-
+puts "** COHORTS **"
 students.each do |cohort, number|
 puts "#{cohort}: #{number} students"
 end
 
 students[:cohort4]=43
-
+puts " "
 puts "We're super succesfully. We've add a new cohort."
 students.each do |cohort, number|
-puts "Here is the list  #{cohort}"
+puts cohort
 end
-
+puts " "
 puts "We're super succesfully. We've got more students"
 students.each do |cohort, number|
 #  puts "Here is the list  #{number}"
@@ -23,13 +23,22 @@ students.each do |cohort, number|
     students[cohort]= number_new.to_i
 end
 
-puts "New list"
+puts "Here is the updated list "
 students.each do |cohort, number|
-puts "Here is the list  #{cohort} : #{number}"
+puts " #{cohort} : #{number}"
+end
+
+students.delete(:cohort2)
+puts " "
+puts "We lost a cohort :("
+students.each do |cohort, number|
+puts "#{cohort} : #{number}"
+end
+
+total_numbers = 0
+students.each do |cohort, number|
+total_numbers = total_numbers + number.to_i
 end
 
 
-# puts "new numbers"
-# students.each do |cohort, number|
-# puts "Here are the numbers #{number}"
-# end
+puts "Day 1 we have #{total_numbers} students. YEAH! "
